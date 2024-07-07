3e7cb5b0ff459300e5ff764386943a404f90b57e
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-07 19:52:25 -0300

    Setting create and list tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='', view=views.tasks, name='tasks')
+    path(route='', view=views.tasks, name='tasks'),
+    path(route='create', view=views.create_tasks, name='create-tasks')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from .models import Tasks
 
 
 def home(request):
     return render(request, 'base.html')
 
 
+def get_tasks():
+    return Tasks.objects.all()
+
+
+def build_context():
+    return {
+        'tasks': get_tasks(),
+        'bg': 'primary',
+    }
+
+
 def tasks(request):
-    return render(request, 'tasks.html')
+    context = build_context()
+
+    return render(
+        request=request, 
+        template_name='tasks.html', 
+        context=context
+    )
+
+
+@require_http_methods(['POST'])
+def create_tasks(request):
+    name = request.POST.get('name')
+    Tasks.objects.create(name=name)
+    context = build_context()
+
+    return render(
+        request=request, 
+        template_name='tasks.html', 
+        context=context
+    )
