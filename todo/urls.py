9bc1e5f928db70d336072cebd956689b17d0806c
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-07 16:12:41 -0300

    Setting views and urls tasks

--- todo/urls.py
+++ todo/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from tasks.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
+    path('', home, name='home')
 ]
