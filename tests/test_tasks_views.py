f5096b70ddac79cb8e690e0f29fea867880d0862
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-08 12:28:40 -0300

    Tests for tasks features done, to undo, and to delete

--- tests/test_tasks_views.py
+++ tests/test_tasks_views.py
@@ -2,8 +2,10 @@ from django.test import Client
 from django.urls import reverse
 
 from tasks.models import Tasks
+from tasks import views
 from .fixtures import (
-    all_tasks, context, tasks_form_data
+    all_tasks, context, tasks_form_data,
+    task_instace
 )
 
 
@@ -33,4 +35,21 @@ class TestTasksViews:
     def test_post_tasks_create_an_object_tasks(self, tasks_form_data):
         url = reverse('create-tasks')
         res = self.c.post(url, tasks_form_data)
+        assert res.status_code == 200
+
+    def test_task_done_view(self, task_instace):
+        url = reverse('done-task', kwargs={'task_id': task_instace.id})
+        res = self.c.put(url)
+        assert res.status_code == 200
+        assert res.context[0]['tasks_done'][0].done == True
+
+    def test_task_undo_view(self, task_instace):
+        url = reverse('undo-task', kwargs={'task_id': task_instace.id})
+        res = self.c.put(url)
+        assert res.status_code == 200
+        assert res.context[0]['tasks'][0].done == False
+
+    def test_task_delete_view(self, task_instace):
+        url = reverse('delete-task', kwargs={'task_id': task_instace.id})
+        res = self.c.delete(url)
         assert res.status_code == 200
