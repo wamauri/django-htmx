50010a3d7cbc1b3eb0110ec73a14b0886bab0718
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-08 11:50:10 -0300

    Setting tasks features done, to undo, and to delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='', view=views.tasks, name='tasks'),
-    path(route='create', view=views.create_tasks, name='create-tasks')
+    path(
+        route='', 
+        view=views.tasks, 
+        name='tasks'
+    ),
+    path(
+        route='create', 
+        view=views.create_tasks, 
+        name='create-tasks'
+    ),
+    path(
+        route='done/<int:task_id>/', 
+        view=views.tasks_done, 
+        name='done-task'
+    ),
+    path(
+        route='delete/<int:task_id>/', 
+        view=views.tasks_delete, 
+        name='delete-task'
+    ),
+    path(
+        route='undo/<int:task_id>/', 
+        view=views.tasks_undo, 
+        name='undo-task'
+    ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -8,17 +8,26 @@ def home(request):
     return render(request, 'base.html')
 
 
-def get_tasks():
-    return Tasks.objects.all()
+def get_tasks(done=None):
+    if done:
+        return Tasks.objects.filter(done=True)
+    return Tasks.objects.exclude(done=True)
 
 
 def build_context():
     return {
         'tasks': get_tasks(),
-        'bg': 'primary',
+        'tasks_done': get_tasks(done=True),
     }
 
 
+def change_task_done(task_id: int, done: bool = False):
+    task = Tasks.objects.get(id=task_id)
+    task.done = done
+    task.save()
+
+
+@require_http_methods(['GET'])
 def tasks(request):
     context = build_context()
 
@@ -33,6 +42,42 @@ def tasks(request):
 def create_tasks(request):
     name = request.POST.get('name')
     Tasks.objects.create(name=name)
+    context = build_context()
+
+    return render(
+        request=request, 
+        template_name='tasks.html', 
+        context=context
+    )
+
+
+@require_http_methods(['PUT'])
+def tasks_done(request, task_id):
+    change_task_done(task_id=task_id, done=True)
+    context = build_context()
+
+    return render(
+        request=request, 
+        template_name='tasks.html', 
+        context=context
+    )
+
+
+@require_http_methods(['PUT'])
+def tasks_undo(request, task_id):
+    change_task_done(task_id=task_id)
+    context = build_context()
+
+    return render(
+        request=request, 
+        template_name='tasks.html', 
+        context=context
+    )
+
+
+@require_http_methods(['DELETE'])
+def tasks_delete(request, task_id):
+    Tasks.objects.get(id=task_id).delete()
     context = build_context()
 
     return render(
