c3c7768bd94c937fddae1dc3a11be802a97103a0
Amauri Clementino <amaurisantospro@gmail.com>
2024-07-08 11:35:12 -0300

    Add done field to tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,12 @@ class Tasks(models.Model):
         verbose_name="Name",
         max_length=255
     )
+    done = models.BooleanField(
+        verbose_name="Done",
+        null=True,
+        blank=True,
+        default=False
+    )
 
     def __str__(self) -> str:
         return self.name
